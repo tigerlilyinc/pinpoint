1d32d0379f4b0f6152277eb3dd25c8d41060b02a
Kevin Meehan <meehan.kevin>
2012-11-21 20:41:13 -0800

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinpoint::Application.config.secret_token = 'df705398b356d82837d4ab7ca32db70442b36a654c185e4c00ff3a4e832c0525859871e70ed2a5e0c5ba3a23bf7afb90fbf8a628c0b68074b39cdb98c5c1212d'
